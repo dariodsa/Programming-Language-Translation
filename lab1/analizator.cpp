21d9a8af37dbe3bb1e94f387d8d4cb470dc833c5
Dario Sindicic <dario.sindicic@gmail.com>
2017-10-19 14:16:53 +0200

    Prvi labos

---
+++ lab1/analizator.cpp
@@ -0,0 +1,324 @@
+#include <stdio.h>
+#include <algorithm>
+#include <math.h>
+#include <string>
+#include <string.h>
+#include <iostream>
+#include <vector>
+#include <map>
+#include <set>
+using namespace std;
+char q[1000];
+int br_automata;
+struct Automat
+{
+	string ime;
+	int id;
+	vector<int>V[1500];
+	vector<char>Va[1500];
+	int pocetno;
+	int prihvatljivo;
+	vector<string>naredbe;
+	set<int>S;
+	void pocetak()
+	{
+		S.clear();
+		S.insert(pocetno);
+	}
+	bool dobar()
+	{
+		set<int>::iterator it;
+		for(it=S.begin();it!=S.end();++it)
+		{
+			if(*it==prihvatljivo)return true;
+		}
+		return false;
+	}
+	bool gotov()
+	{
+		return S.size()==0;
+	}
+	void prijelaz(char w)
+	{
+		set<int>::iterator it;
+		vector<int>temp;
+		//if(w==111){for(it=S.begin();it!=S.end();++it){printf("%d, ",*it);}printf("\n");}
+		//cout<<"Znak ide u "<<w<<endl;		
+		while(true)
+		{
+			int size=S.size();
+			set<int>::iterator it;
+			set<int>S1;
+			S1.clear();
+			bool add=false;
+			for(it=S.begin();it!=S.end();++it)
+			{
+				int br=*it;
+				for(int i=0;i<V[br].size();++i)
+				{
+					if(Va[br][i]=='¸')
+					{
+						S1.insert(V[br][i]);
+						//if(V[br][i]==382)printf("VEER %d %d\n",br,i);
+					}
+				}
+			}
+			for(it=S1.begin();it!=S1.end();++it)S.insert(*it);
+			if(S.size()==size)break;
+		}
+	/*	printf("Epsilon okruzenje %c\n",w);
+		for(it=S.begin();it!=S.end();++it){printf("a %d, ",*it);}printf("\n");*/
+		for(it=S.begin();it!=S.end();++it)
+		{
+			int i=*it;
+			bool dobar=false;
+			for(int j=0;j<V[i].size();++j)
+			{
+				if(Va[i][j]==w)dobar=true;
+			}
+			if(!dobar)temp.push_back(*it);
+		}
+		for(int i=0;i<temp.size();++i)S.erase(temp[i]);
+		temp.clear();
+		int kol=0;
+		int size=S.size();
+		//set<int>::iterator it;
+		set<int>S1;
+		bool add=false;
+		set<int>brisi;
+		S1.clear();
+		for(it=S.begin();it!=S.end();++it)
+		{
+			int br=*it;
+			for(int i=0;i<V[br].size();++i)
+			{
+				if(Va[br][i]==w)
+				{
+					//if(V[br][i]==382)printf("VEER %d %d\n",br,i);
+					S1.insert(V[br][i]);
+					brisi.insert(br);
+					
+					//printf("Dodajem %d brisem %d\n",V[br][i],br);
+					//printf("Fonud %c\n",w);
+				}
+			}
+		}
+		/*printf("Pravo okruzenje %c\n",w);
+		for(it=S.begin();it!=S.end();++it){printf("a %d, ",*it);}printf("\n");*/
+		for(it=brisi.begin();it!=brisi.end();++it)S.erase(*it);
+		brisi.clear();
+		for(it=S1.begin();it!=S1.end();++it)S.insert(*it);
+		S1.clear();
+		while(true)
+		{
+			int size=S.size();
+			set<int>::iterator it;
+			set<int>S1;
+			bool add=false;
+			for(it=S.begin();it!=S.end();++it)
+			{
+				int br=*it;
+				for(int i=0;i<V[br].size();++i)
+				{
+					if(Va[br][i]=='¸')
+						S1.insert(V[br][i]);
+				}
+			}
+			for(it=S1.begin();it!=S1.end();++it)S.insert(*it);
+			if(S.size()==size)break;
+		}
+		//for(it=S.begin();it!=S.end();++it){printf("a %d, ",*it);}printf("\n");
+		
+	}
+	
+};
+int BR_REDAKA=1;
+string STANJE="S_pocetno";
+int BR=-1;
+string izraz="";
+char t[14400];
+string kod="";
+void obrada(int poc,string naredba,string query);
+void naredbeAll(int poc,vector<string>naredbe,string query)
+{
+	string po="";
+	for(int i=0;i<naredbe.size();++i)
+	{
+		//cout<<naredbe[i]<< " --> izvrsavam"<<endl;
+		if(naredbe[i][0]=='V' && naredbe[i][1]=='R')
+		{
+			int broj=0;
+			for(int k=strlen("VRATI_SE ");k<naredbe[i].length();++k)broj=(broj*10)+(naredbe[i][k]-'0');
+			for(int k=poc;k<poc+broj;++k)po+=kod[k];
+			query=po;
+		}
+		
+	}
+	for(int i=0;i<naredbe.size();++i)obrada(poc,naredbe[i],query);
+}
+void obrada(int poc, string naredba,string query)
+{
+	if(naredba=="-")
+	{
+		return;
+	}
+	if(naredba=="NOVI_REDAK")
+	{
+		BR_REDAKA++;
+		return;
+	}
+	int found1=naredba.find("UDJI_U_STANJE ");
+	int found2=naredba.find("VRATI_SE ");
+	if(found1!=string::npos)
+	{
+		string stanje="";
+		for(int i=strlen("UDJI_U_STANJE ");i<naredba.length();++i)stanje+=naredba[i];
+		//nesto napravi
+		//cout<<"IDEM U "<<stanje<<endl;
+		STANJE=stanje;
+		return;
+	}
+	if(found2!=string::npos)
+	{
+		int broj=0;
+		//printf("SRANJE\n");
+		for(int i=strlen("VRATI_SE ");i<naredba.length();++i)broj=(broj*10)+(naredba[i]-'0');
+		BR=broj;
+		return;
+	}
+	else cout<<naredba<<" "<<BR_REDAKA<<" "<<query<<endl;	
+	
+}
+vector<Automat>automati;
+int a,b,c,d;
+int main()
+{
+	FILE *pfile;
+	pfile=fopen("tablica.txt","r");
+	fscanf(pfile,"%d",&br_automata);
+	for(int z=0;z<br_automata;++z)
+	{
+		Automat A=Automat();
+		fscanf(pfile,"%s",q);
+		string ime=q;
+		fscanf(pfile,"%d",&a);
+		
+		//prijelazi
+		for(int i=0;i<a;++i)
+		{
+			fscanf(pfile,"%d%d%d",&b,&c,&d);
+			
+			A.V[b].push_back(c);
+			A.Va[b].push_back(d);
+		}
+		
+		fscanf(pfile,"%d%d",&b,&c);
+		//pocetno stanje // prihvatljivo
+		fscanf(pfile,"%d\n",&d);
+		for(int i=0;i<d;++i)
+		{
+			
+			fgets(q,sizeof q, pfile);
+			string w="";
+			for(int j=0;j<strlen(q);++j)if(q[j]!='\n')w+=q[j];
+			//string w=q;
+			//cout<<"Naredba "<<d<<" "<<i<<" "<<w<<endl;
+			//dodaj u naredbe
+			A.naredbe.push_back(w);
+		}
+		A.ime=ime;
+		A.id=z;
+		A.pocetno=b;
+		A.prihvatljivo=c;
+		A.pocetak();
+		automati.push_back(A);
+	}
+	fscanf(pfile,"%s",q);
+	STANJE=q;
+	//vector<string>kod;
+	
+	string k;
+	while(getline(cin,k))
+	{
+		kod+=k+"\n";
+	//	cout<<k<<endl;
+	}
+	int id2=4545646;
+	int id;
+	int kol=0;
+	int lastOne=0;
+	string query="";
+	int i=0;
+	//cout<<kod<<endl;
+	for(i=0;i<kod.length();++i)
+	{
+		//cout<<i<<" "<<STANJE<<" "<<kod[i]<<endl;
+		string query="";
+		int last_Automat=-1;
+		int last_id=-1;
+		for(int j=i;j<kod.length();++j)
+		{
+			//printf("a %d\n",j);
+			for(int k=0;k<automati.size();++k)
+			{
+				if(automati[k].ime==STANJE && !automati[k].gotov())
+				{
+					automati[k].prijelaz(kod[j]);
+					if(automati[k].dobar() && last_id!=j)last_id=j,last_Automat=k;
+				}
+			}
+		}
+		if(last_id==-1)
+		{
+			//pogreska
+			//printf("ERROR leksicka greska\n");
+			for(int j=0;j<automati.size();++j)automati[j].pocetak();
+			continue;
+		}
+		else
+		{
+			//printf("Prihvacen od %d automata.%d %d\n",last_Automat,i,last_id);
+			string ki="";
+			for(int k=i;k<=last_id;++k)ki+=kod[k];
+			naredbeAll(i,automati[last_Automat].naredbe,ki);
+			if(BR!=-1){i=i+BR-1;BR=-1;}
+			else i=last_id;
+			for(int j=0;j<automati.size();++j)automati[j].pocetak();
+		}
+	}
+	
+	
+	/*for( i=0;i<kod.length();++i)
+	{
+		id=-1;
+		cout<<"Stanje"<<STANJE<<endl;
+		for(int j=0;j<automati.size();++j)
+		{
+			if(automati[j].ime==STANJE)
+			{
+				automati[j].prijelaz(kod[i]);
+				if(automati[j].gotov()==false)
+				{
+					if(id==-1)id=j;
+				}
+			}
+		}
+		printf("%d %d %c -> %d \n",i,id,kod[i],lastOne);
+		if(id==-1)
+		{
+			printf("Probam %d\n",id2);
+			naredbeAll(automati[id2].naredbe,query);
+			query="";
+			if(BR!=-1)i=lastOne+BR,BR=-1,lastOne=i;
+			else lastOne=i;
+			--i;
+			//printf("NASAO \n");
+			//cout<<automati[id2].ime<<" "<<i<<" "<<kod[i]<<endl;
+			for(int j=0;j<automati.size();++j)automati[j].pocetak();
+			continue;
+		}
+		else query+=kod[i];
+		id2=id;
+	}*/
+	return 0;
+}
---
+++ lab1/generator.cpp
@@ -0,0 +1,407 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <algorithm>
+#include <string.h>
+#include <string>
+#include <iostream>
+#include <map>
+#include <vector>
+using  namespace std;
+map<string,string>mapa;
+string q;
+vector<string>regularne_definicije;
+vector<string>stanja_analizatora;
+vector<string>lek_jedinke;
+vector<string>pravila;
+map<int,string >ulazak_u_lek_analizatora;
+map<int,vector<string> >pravila_lek_analizatora;
+void obradi_reg()
+{
+	string ime="";
+	string definicija="";
+	int i;
+	for(i=1;i<q.length();++i)
+	{
+		if(q[i]=='}')break;
+		ime+=q[i];
+	}
+	regularne_definicije.push_back(ime);
+	for(int j=i+2;j<q.length();++j)
+		definicija+=q[j];
+	cout<<q<<endl;
+	cout<<ime<<endl;
+	mapa[ime]=definicija;
+	cout<<"+++"<<mapa[ime]<<endl;
+}
+void obrada_stanja_lek_ana()
+{
+	int i=3;
+	string w="";
+	for(i=3;i<q.length();++i)
+	{
+		if(q[i]==' ')
+		{
+			stanja_analizatora.push_back(w);
+			w="";
+		}
+		else w+=q[i];
+	}
+	stanja_analizatora.push_back(w);
+}
+void obrada_imena_lek_jed()
+{
+	int i=3;
+	string w="";
+	for(i=3;i<q.length();++i)
+	{
+		if(q[i]==' ')
+		{
+			lek_jedinke.push_back(w);
+			w="";
+		}
+		else w+=q[i];
+	}
+	lek_jedinke.push_back(w);
+}
+string obrada_pravila()
+{
+	string ime="";
+	int i;
+	for(i=1;i<q.length();++i)
+	{
+		if(q[i]=='>')break;
+		ime+=q[i];
+	}
+	string pravilo="";
+	for(int j=i+1;j<q.length();++j)
+		pravilo+=q[j];
+	//cout<<"Ulazak: "<<pravilo<<endl;
+	ulazak_u_lek_analizatora[pravila.size()]=pravilo;
+	return ime;
+}
+string obrada_reg_definicija(string def)
+{
+	//string def=mapa[regularne_definicije[i]];
+	bool jos=true;
+	//int br=0;
+	while(jos)
+	{
+		jos=false;
+		for(int j=0;j<regularne_definicije.size();++j)
+		{
+			int found=def.find(regularne_definicije[j]);
+			if(found!=string::npos)
+			{
+				//cout<<"Nasao "<<regularne_definicije[j]<< " "<<mapa[regularne_definicije[j]];
+				//cout<<def<<" -->"<<endl;
+				string nova_def="("+mapa[regularne_definicije[j]]+")";
+				def.replace(found-1,regularne_definicije[j].size()+2,nova_def);
+				//cout<<def<<" -->"<<endl;
+				jos=true;
+			}
+		}
+	}
+	return def;
+}
+struct ParStanja
+{
+	int lijevo_stanje;
+	int desno_stanje;
+};
+struct Automat
+{
+	int prihvatljivo_stanje;
+	int pocetno_stanje;
+	int br_stanja;
+	//char prijelazi[500][500];
+	Automat()
+	{
+	}
+	vector<int>Q1;
+	vector<int>Q2;
+	vector<int>P;
+	void brisi()
+	{
+		//memset(prijelazi,0,sizeof prijelazi);
+		Q1.clear();
+		Q2.clear();
+		P.clear();
+	}
+	void dodaj_prijelaz(int stanjeA,int stanjeB,int znak)
+	{
+	//	cout<<"Znak "<<znak<<" "<<stanjeA<<" "<<stanjeB<<endl;
+	//	printf("%d\n",Q1.size());
+		//A->prijelazi[stanjeA][stanjeB]=znak;
+		
+		Q1.push_back(stanjeA);
+		Q2.push_back(stanjeB);
+		P.push_back(znak);
+		
+	}
+	void dodaj_epsilon_prijelaz(int stanjeA,int stanjeB)
+	{
+		dodaj_prijelaz(stanjeA,stanjeB,'¸');
+	}
+	int novo_stanje()
+	{
+		br_stanja++;
+		return br_stanja-1;
+	}
+	string obrada(string izraz)
+	{
+		string ans="";
+		int kol=0;
+		int br_zagrada;
+		for(int i=0;i<izraz.length();++i)
+		{
+			if(izraz[i]=='(' && je_operator(izraz,i))
+				br_zagrada++;
+			else if(izraz[i]==')' && je_operator(izraz,i))
+				br_zagrada--;
+			else if(br_zagrada==0)
+				++kol;
+		}
+		if(kol==0)for(int i=1;i<izraz.length()-1;++i)ans+=izraz[i];
+		else ans=izraz;
+		return ans;
+	}
+	ParStanja pretvori(string izraz)
+	{
+		int br_zagrada=0;
+		//niz izbori
+		vector<string>izbori;
+		int zadnji=0;
+		int br_operatora_izbora=0;
+		//izraz=obrada(izraz);
+		for(int i=0;i<izraz.length();++i)
+		{
+			printf("(%d,%d)\n",i,br_zagrada);
+			if(izraz[i]=='(' && je_operator(izraz,i))
+				br_zagrada++;
+			else if(izraz[i]==')' && je_operator(izraz,i))
+				br_zagrada--;
+			else if(br_zagrada==0 && izraz[i]=='|' && je_operator(izraz,i))
+			{
+				++br_operatora_izbora;
+				printf("Operator %d %d\n",i,izraz[i]);
+				//grupiraj lijevi negrupirani dio niza znakova izraz u niz izbori
+				string temp="";
+				for(int k=zadnji;k<i;++k)temp+=izraz[k];
+				zadnji=i+1;
+				izbori.push_back(temp);
+			}
+		}
+		/*ako je pronadjen barem jedan operator izbora
+			 grupiraj preostali negrupirani dio niza znakova izraz u niz izbori*/
+		if(br_operatora_izbora>0)
+		{
+			string temp="";
+			for(int k=zadnji;k<izraz.length();++k)temp+=izraz[k];
+			izbori.push_back(temp);
+			printf("Nasao\n");
+			for(int z=0;z<izbori.size();++z)
+			{
+				cout<<izbori[z]<<", ";
+			}
+			printf("\n");
+		}
+		int lijevo_stanje=novo_stanje();
+		int desno_stanje=novo_stanje();
+		if(br_operatora_izbora>0)
+		{
+			for(int i=0;i<izbori.size();++i)
+			{
+				ParStanja privremeno=pretvori(izbori[i]);
+				dodaj_epsilon_prijelaz(lijevo_stanje,privremeno.lijevo_stanje);
+				dodaj_epsilon_prijelaz(privremeno.desno_stanje,desno_stanje);
+			}
+		}
+		else
+		{
+			printf("Usao\n");
+			bool prefiksirano=false;
+			int zadnje_stanje=lijevo_stanje;
+			for(int i=0;i<izraz.length();++i)
+			{
+				int a,b;
+				if(prefiksirano)
+				{
+					//1
+					prefiksirano=false;
+					char prijelazni_znak;
+					if(izraz[i]=='t')
+						prijelazni_znak='\t';
+					else if(izraz[i]=='n')
+						prijelazni_znak='\n';
+					else if(izraz[i]=='_')
+					{
+						prijelazni_znak=' ';
+					}
+					else prijelazni_znak=izraz[i];
+					a=novo_stanje();
+					b=novo_stanje();
+					printf("Prilazni znak %c %d\n",prijelazni_znak,prijelazni_znak);
+					cout<<izraz<<endl;
+					dodaj_prijelaz(a,b,prijelazni_znak);
+				}
+				else
+				{
+					printf("ioi %c\n",izraz[i]);
+					//2
+					if(izraz[i]=='\\')
+					{
+						prefiksirano=true;
+						continue;
+					}
+					if(izraz[i]!='(')
+					{
+						//2A
+						a=novo_stanje();
+						b=novo_stanje();
+						printf("Dodan prijelaz %c\n",izraz[i]);
+						cout<<izraz<<endl;
+						if(izraz[i]=='¸')
+							dodaj_epsilon_prijelaz(a,b);
+						else 
+							dodaj_prijelaz(a,b,izraz[i]);
+					}
+					else
+					{
+						//2B
+						int j;//*pronadji odgovarajucu zatvorenu zagradu*
+						string izraz2="";
+						int kol=0;
+						for(j=i+1;j<izraz.length();++j)
+							if(izraz[j]=='(' && je_operator(izraz,j)){++kol;izraz2+=izraz[j];}
+							else if(izraz[j]==')' && je_operator(izraz,j) && kol>0){--kol;izraz2+=izraz[j];}
+							else if(izraz[j]==')' && je_operator(izraz,j) && kol==0)break;
+							else izraz2+=izraz[j];
+						cout<<"Novi bolji "<<izraz2<<endl;
+						ParStanja privremeno=pretvori(izraz2);
+						a=privremeno.lijevo_stanje;
+						b=privremeno.desno_stanje;
+						i=j;
+					}
+				}
+				
+				if(i+1<izraz.length() && izraz[i+1]=='*')
+				{
+					int x=a;
+					int y=b;
+					a=novo_stanje();
+					b=novo_stanje();
+					dodaj_epsilon_prijelaz(a,x);
+					dodaj_epsilon_prijelaz(y,b);
+					dodaj_epsilon_prijelaz(a,b);
+					dodaj_epsilon_prijelaz(y,x);
+					++i;
+				}
+				dodaj_epsilon_prijelaz(zadnje_stanje,a);
+				zadnje_stanje=b;
+			}
+			dodaj_epsilon_prijelaz(zadnje_stanje,desno_stanje);
+		}
+		ParStanja rez;
+		rez.lijevo_stanje=lijevo_stanje;
+		rez.desno_stanje=desno_stanje;
+		return rez;			
+	}
+	bool je_operator(string izraz,int i)
+	{
+		int br=0;
+		while(i>=0 && izraz[i-1]=='\\')
+		{
+			++br;
+			--i;
+		}
+		return br%2==0;
+	}
+};
+
+
+
+int main()
+{
+	bool aktivna_naredba=false;
+	string ime_pravila;
+	FILE *pfile=fopen("tablica.txt","w");
+	while(getline(cin,q))
+	{
+		if(q[0]=='{' && !aktivna_naredba)
+		{
+			obradi_reg();
+		}
+		else if(q[0]=='%' && q[1]=='X')
+		{
+			obrada_stanja_lek_ana();
+		}
+		else if(q[0]=='%' && q[1]=='L')
+		{
+			obrada_imena_lek_jed();
+		}
+		else
+		{
+			//pravila lek analizatora
+			if(q[0]=='}')
+			{
+				aktivna_naredba=false;
+				continue;
+			}
+			if(q[0]=='<')
+			{
+				ime_pravila=obrada_pravila();
+				pravila.push_back(ime_pravila);
+				aktivna_naredba=true;
+			}
+			else if(q[0]!='{')
+			{
+				pravila_lek_analizatora[pravila.size()-1].push_back(q);
+			}
+		}
+	}
+	//printf("Hk");
+	vector<Automat>automati;
+	for(int i=0;i<regularne_definicije.size();++i)
+	{
+		//cout<<obrada_reg_definicija(mapa[regularne_definicije[i]])<<endl;
+		mapa[regularne_definicije[i]]=obrada_reg_definicija(mapa[regularne_definicije[i]]);
+		/*Automat A;
+		A.br_stanja=0;
+		ParStanja rezultat=pretvori(mapa[regularne_definicije[i]],A);
+		automati.push_back(A);*/
+	}
+	for(int i=0;i<pravila.size();++i)
+	{
+		ulazak_u_lek_analizatora[i]=obrada_reg_definicija(ulazak_u_lek_analizatora[i]);
+		
+		Automat A=Automat();
+		A.brisi();
+		A.br_stanja=0;
+		cout<<"Provjera: "<<ulazak_u_lek_analizatora[i]<<endl;
+		
+		ParStanja rezultat=A.pretvori(ulazak_u_lek_analizatora[i]);
+		A.pocetno_stanje=rezultat.lijevo_stanje;
+		A.prihvatljivo_stanje=rezultat.desno_stanje;
+		//printf("%d a\n",A.br_stanja);
+		automati.push_back(A);
+	}
+	freopen("tablica.txt","w",stdout);
+	cout<<automati.size()<<endl;
+	for(int i=0;i<automati.size();++i)
+	{
+		cout<<pravila[i]<<endl;
+		cout<<automati[i].Q1.size()<<endl;
+		for(int j=0;j<automati[i].Q1.size();++j)
+		{
+			cout<<automati[i].Q1[j]<<" "<<automati[i].Q2[j]<<" "<<automati[i].P[j]<<endl;
+		}
+		cout<<automati[i].pocetno_stanje<<" "<<automati[i].prihvatljivo_stanje<<endl;
+		cout<<pravila_lek_analizatora[i].size()<<endl;
+		for(int j=0;j<pravila_lek_analizatora[i].size();++j)
+		{
+			cout<<pravila_lek_analizatora[i][j]<<endl;
+		}
+	}
+	cout<<stanja_analizatora[0]<<endl;
+	return 0;
+}

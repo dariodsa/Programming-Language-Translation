cfd5cd1e59d82943128c564f1bc228ea0b44443b
Dario Sindicic <dario.sindicic@gmail.com>
2018-01-28 15:24:49 +0100

    travisTest 1

--- lab4/evaluator.bat
+++ lab4/evaluator.bat
@@ -1,15 +1,15 @@
 cd ..
 cd lab1
-analizator.exe < "../lab3/kodovi/%1" > "../lab2/analizator/out.txt"
+./analizator.exe < "../lab3/kodovi/%1" > "../lab2/analizator/out.txt"
 cd ..
 cd lab2
 cd analizator
-analizator.exe < "out.txt" > "../../lab3/out.txt"
+./analizator.exe < "out.txt" > "../../lab3/out.txt"
 cd ..
 cd ..
 cd lab3
-generator <out.txt
+./generator <out.txt
 cd ..
 cd lab4
-generator <"../lab3/out.txt" >out.txt
-node main.js out.txt >iz.txt
+./generator <"../lab3/out.txt" >out.txt
+./node main.js out.txt >iz.txt
